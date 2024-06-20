e522e71b6d34ae4b934daa80987e2380df13564b
Simon <yash1441@yahoo.com>
2024-06-20 12:35:59 +0530

    Update localization string

--- components/buttons/deny-suggestion.js
+++ components/buttons/deny-suggestion.js
@@ -1,5 +1,6 @@
 const { EmbedBuilder, userMention } = require('discord.js');
 require('dotenv').config();
+const localization = require('../../utils/localization');
 
 module.exports = {
     cooldown: 10,
@@ -11,7 +12,7 @@ module.exports = {
 
         const embed = interaction.message.embeds[0];
 
-        const deniedEmbed = EmbedBuilder.from(embed).setColor(process.env.EMBED_COLOR_DENIED).addFields({ name: 'Decision', value: userMention(interaction.user.id), inline:true });
+        const deniedEmbed = EmbedBuilder.from(embed).setColor(process.env.EMBED_COLOR_DENIED).addFields({ name: localization.decision[interaction.locale] ?? localization.decision["en-US"], value: userMention(interaction.user.id), inline:true });
 
         await interaction.message.edit({ embeds: [deniedEmbed], components: [] }).then(() => interaction.deleteReply());
     },
