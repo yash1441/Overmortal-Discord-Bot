08aca934dd10778cd752798ade50d470c00e7348
Simon <yash1441@yahoo.com>
2024-07-03 14:33:00 +0530

    Add X as platform

--- components/buttons/creator-application.js
+++ components/buttons/creator-application.js
@@ -1,133 +1,223 @@
-const { StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ComponentType, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, EmbedBuilder, ButtonBuilder, ButtonStyle, bold, inlineCode, codeBlock, channelMention, userMention } = require('discord.js');
-const lark = require('../../utils/lark');
-require('dotenv').config();
+const {
+	StringSelectMenuBuilder,
+	StringSelectMenuOptionBuilder,
+	ComponentType,
+	ActionRowBuilder,
+	ModalBuilder,
+	TextInputBuilder,
+	TextInputStyle,
+	EmbedBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	bold,
+	inlineCode,
+	codeBlock,
+	channelMention,
+	userMention,
+} = require("discord.js");
+const lark = require("../../utils/lark");
+require("dotenv").config();
 
-const servers = require('../../utils/servers');
-const localization = require('../../utils/localization');
+const servers = require("../../utils/servers");
+const localization = require("../../utils/localization");
 
 module.exports = {
-    cooldown: 60,
-    data: {
-        name: 'creator-application',
-    },
-    async execute(interaction) {
-        await interaction.deferReply({ ephemeral: true });
+	cooldown: 60,
+	data: {
+		name: "creator-application",
+	},
+	async execute(interaction) {
+		await interaction.deferReply({ ephemeral: true });
 
-        const creatorData = {
-            guildId: interaction.guildId,
-            userId: interaction.user.id,
-            name: interaction.user.username,
-            region: servers[interaction.guildId].region
-        };
+		const creatorData = {
+			guildId: interaction.guildId,
+			userId: interaction.user.id,
+			name: interaction.user.username,
+			region: servers[interaction.guildId].region,
+		};
 
-        const creatorModal = new ModalBuilder().setCustomId('creator-modal');
+		const creatorModal = new ModalBuilder().setCustomId("creator-modal");
 
-        const platformSelectMenu = new StringSelectMenuBuilder()
-            .setCustomId('creator-platform')
-            .setPlaceholder(localization.select_platform[interaction.locale] ?? localization.select_platform["en-US"])
-            .addOptions(
-                new StringSelectMenuOptionBuilder()
-                    .setLabel('YouTube')
-                    .setValue('YouTube'),
-                new StringSelectMenuOptionBuilder()
-                    .setLabel('YouTube Shorts')
-                    .setValue('YouTube Shorts'),
-                new StringSelectMenuOptionBuilder()
-                    .setLabel('TikTok')
-                    .setValue('TikTok'),
-                new StringSelectMenuOptionBuilder()
-                    .setLabel('Twitch')
-                    .setValue('Twitch'),
-            );
+		const platformSelectMenu = new StringSelectMenuBuilder()
+			.setCustomId("creator-platform")
+			.setPlaceholder(
+				localization.select_platform[interaction.locale] ??
+					localization.select_platform["en-US"]
+			)
+			.addOptions(
+				new StringSelectMenuOptionBuilder()
+					.setLabel("YouTube")
+					.setValue("YouTube"),
+				new StringSelectMenuOptionBuilder()
+					.setLabel("YouTube Shorts")
+					.setValue("YouTube Shorts"),
+				new StringSelectMenuOptionBuilder()
+					.setLabel("TikTok")
+					.setValue("TikTok"),
+				new StringSelectMenuOptionBuilder()
+					.setLabel("Twitch")
+					.setValue("Twitch"),
+				new StringSelectMenuOptionBuilder()
+					.setLabel("X")
+					.setValue("X")
+			);
 
-        const row = new ActionRowBuilder().addComponents(platformSelectMenu);
+		const row = new ActionRowBuilder().addComponents(platformSelectMenu);
 
-        await interaction.editReply({
-            content: bold(localization.select_submission_platform[interaction.locale] ?? localization.select_submission_platform["en-US"]),
-            components: [row],
-        });
+		await interaction.editReply({
+			content: bold(
+				localization.select_submission_platform[interaction.locale] ??
+					localization.select_submission_platform["en-US"]
+			),
+			components: [row],
+		});
 
-        const platformReply = await interaction.fetchReply();
+		const platformReply = await interaction.fetchReply();
 
-        const collector = platformReply.createMessageComponentCollector({
-            time: 10_000,
-            componentType: ComponentType.StringSelect
-        });
+		const collector = platformReply.createMessageComponentCollector({
+			time: 10_000,
+			componentType: ComponentType.StringSelect,
+		});
 
-        collector.on('collect', async (platformInteraction) => {
-            creatorData.platform = platformInteraction.values[0];
+		collector.on("collect", async (platformInteraction) => {
+			creatorData.platform = platformInteraction.values[0];
 
-            const creatorModalChannel = new ActionRowBuilder().addComponents(new TextInputBuilder().setCustomId('channel').setLabel(JSON.parse(JSON.stringify(localization).replaceAll('{{platform}}', creatorData.platform)).channel_link[interaction.locale] ?? JSON.parse(JSON.stringify(localization).replaceAll('{{platform}}', creatorData.platform)).channel_link["en-US"]).setStyle(TextInputStyle.Short));
-            const creatorModalSubs = new ActionRowBuilder().addComponents(new TextInputBuilder().setCustomId('subs').setLabel(localization.channel_subscribers[interaction.locale] ?? localization.channel_subscribers["en-US"]).setStyle(TextInputStyle.Short));
-            const creatorModalIntro = new ActionRowBuilder().addComponents(new TextInputBuilder().setCustomId('intro').setLabel(localization.brief_introduction[interaction.locale] ?? localization.brief_introduction["en-US"]).setStyle(TextInputStyle.Paragraph));
+			const creatorModalChannel = new ActionRowBuilder().addComponents(
+				new TextInputBuilder()
+					.setCustomId("channel")
+					.setLabel(
+						JSON.parse(
+							JSON.stringify(localization).replaceAll(
+								"{{platform}}",
+								creatorData.platform
+							)
+						).channel_link[interaction.locale] ??
+							JSON.parse(
+								JSON.stringify(localization).replaceAll(
+									"{{platform}}",
+									creatorData.platform
+								)
+							).channel_link["en-US"]
+					)
+					.setStyle(TextInputStyle.Short)
+			);
+			const creatorModalSubs = new ActionRowBuilder().addComponents(
+				new TextInputBuilder()
+					.setCustomId("subs")
+					.setLabel(
+						localization.channel_subscribers[interaction.locale] ??
+							localization.channel_subscribers["en-US"]
+					)
+					.setStyle(TextInputStyle.Short)
+			);
+			const creatorModalIntro = new ActionRowBuilder().addComponents(
+				new TextInputBuilder()
+					.setCustomId("intro")
+					.setLabel(
+						localization.brief_introduction[interaction.locale] ??
+							localization.brief_introduction["en-US"]
+					)
+					.setStyle(TextInputStyle.Paragraph)
+			);
 
-            creatorModal.addComponents(creatorModalChannel, creatorModalSubs, creatorModalIntro);
-            creatorModal.setTitle(creatorData.platform + ' Creator');
+			creatorModal.addComponents(
+				creatorModalChannel,
+				creatorModalSubs,
+				creatorModalIntro
+			);
+			creatorModal.setTitle(creatorData.platform + " Creator");
 
-            await platformInteraction.showModal(creatorModal);
+			await platformInteraction.showModal(creatorModal);
 
-            const modalReply = await platformInteraction.awaitModalSubmit({
-                time: 60_000,
-                filter: (modalInteraction) => modalInteraction.user.id === platformInteraction.user.id
-            }).catch(() => {
-                interaction.editReply({
-                    content: localization.form_expired[interaction.locale] ?? localization.form_expired["en-US"], components: []
-                });
-                setTimeout(() => interaction.deleteReply(), 10_000);
-            });
+			const modalReply = await platformInteraction
+				.awaitModalSubmit({
+					time: 60_000,
+					filter: (modalInteraction) =>
+						modalInteraction.user.id ===
+						platformInteraction.user.id,
+				})
+				.catch(() => {
+					interaction.editReply({
+						content:
+							localization.form_expired[interaction.locale] ??
+							localization.form_expired["en-US"],
+						components: [],
+					});
+					setTimeout(() => interaction.deleteReply(), 10_000);
+				});
 
-            collector.stop();
+			collector.stop();
 
-            if (!creatorData.platform || !modalReply) return;
+			if (!creatorData.platform || !modalReply) return;
 
-            await modalReply.reply({ content: bold(creatorData.platform), ephemeral: true })
+			await modalReply.reply({
+				content: bold(creatorData.platform),
+				ephemeral: true,
+			});
 
-            await modalReply.deleteReply();
+			await modalReply.deleteReply();
 
-            creatorData.channel = modalReply.fields.getTextInputValue('channel');
-            creatorData.subs = modalReply.fields.getTextInputValue('subs');
-            creatorData.intro = modalReply.fields.getTextInputValue('intro');
+			creatorData.channel =
+				modalReply.fields.getTextInputValue("channel");
+			creatorData.subs = modalReply.fields.getTextInputValue("subs");
+			creatorData.intro = modalReply.fields.getTextInputValue("intro");
 
-            await interaction.editReply({
-                content: (localization.application_submitted[interaction.locale] ?? localization.application_submitted["en-US"]) + '\nPlatform ' + inlineCode(creatorData.platform) + '\nChannel ' + inlineCode(creatorData.channel) + '\nSubscribers ' + inlineCode(creatorData.subs),
-                components: []
-            });
+			await interaction.editReply({
+				content:
+					(localization.application_submitted[interaction.locale] ??
+						localization.application_submitted["en-US"]) +
+					"\nPlatform " +
+					inlineCode(creatorData.platform) +
+					"\nChannel " +
+					inlineCode(creatorData.channel) +
+					"\nSubscribers " +
+					inlineCode(creatorData.subs),
+				components: [],
+			});
 
-            const records = await lark.listRecords(
-                process.env.CREATOR_BASE,
-                process.env.APPLICATION_TABLE,
-                { filter: `CurrentValue.[Discord ID] = "${creatorData.userId}"` }
-            );
+			const records = await lark.listRecords(
+				process.env.CREATOR_BASE,
+				process.env.APPLICATION_TABLE,
+				{
+					filter: `CurrentValue.[Discord ID] = "${creatorData.userId}"`,
+				}
+			);
 
-            if (records.total) return console.log(creatorData.name, ' already submitted.');
+			if (records.total)
+				return console.log(creatorData.name, " already submitted.");
 
-            const success = await lark.createRecord(
-                process.env.CREATOR_BASE,
-                process.env.APPLICATION_TABLE,
-                {
-                    "fields": {
-                        "Discord ID": creatorData.userId,
-                        "Discord Name": creatorData.name,
-                        "Platform": creatorData.platform,
-                        "Channel": {
-                            link: creatorData.channel,
-                            text: creatorData.channel,
-                        },
-                        "Region": creatorData.region,
-                        "Subscribers": parseInt(creatorData.subs) ?? 0,
-                        "Introduction": creatorData.intro
-                    }
-                }
-            );
+			const success = await lark.createRecord(
+				process.env.CREATOR_BASE,
+				process.env.APPLICATION_TABLE,
+				{
+					fields: {
+						"Discord ID": creatorData.userId,
+						"Discord Name": creatorData.name,
+						Platform: creatorData.platform,
+						Channel: {
+							link: creatorData.channel,
+							text: creatorData.channel,
+						},
+						Region: creatorData.region,
+						Subscribers: parseInt(creatorData.subs) ?? 0,
+						Introduction: creatorData.intro,
+					},
+				}
+			);
 
-            if (!success) return console.warn('Could not create record.');
-        });
+			if (!success) return console.warn("Could not create record.");
+		});
 
-        collector.on('end', (collected, reason) => {
-            if (reason === 'time' && !collected.size) {
-                interaction.editReply({ content: localization.selection_expired[interaction.locale] ?? localization.selection_expired["en-US"], components: [] });
-                setTimeout(() => interaction.deleteReply(), 10_000);
-            };
-        });
-    },
+		collector.on("end", (collected, reason) => {
+			if (reason === "time" && !collected.size) {
+				interaction.editReply({
+					content:
+						localization.selection_expired[interaction.locale] ??
+						localization.selection_expired["en-US"],
+					components: [],
+				});
+				setTimeout(() => interaction.deleteReply(), 10_000);
+			}
+		});
+	},
 };
